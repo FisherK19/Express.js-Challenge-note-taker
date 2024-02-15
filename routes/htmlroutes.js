87557a4f9a5ec37e07e06125037984a33b1aa9af
kristie fisher <fisherkristie19@icloud.com>
2024-02-15 17:37:45 -0500

    changing files

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,15 +2,17 @@
 const path = require('path');
 const router = require('express').Router();
 
-// GET request for index.html
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
-// GET request for notes.html
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-module.exports = router;
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+
+module.exports = router;
