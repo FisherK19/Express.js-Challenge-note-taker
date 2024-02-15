efbf4901c7dc6068656b587f450255bbdb247d29
kristie fisher <fisherkristie19@icloud.com>
2024-02-15 17:28:04 -0500

    trying to fix the db path

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -2,17 +2,15 @@
 const path = require('path');
 const router = require('express').Router();
 
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-});
-
+// GET request for index.html
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+// GET request for notes.html
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-
 module.exports = router;
+
