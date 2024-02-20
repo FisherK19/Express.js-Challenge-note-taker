deb4a5c51e71e6220c0042ab34139f82db48e9a6
kristie fisher <fisherkristie19@icloud.com>
2024-02-20 09:37:24 -0500

    fixing db file uploading from endpoint

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000; 
-const routes = require('./routes/routes');
+const PORT = process.env.PORT || 3000;
 const path = require('path');
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
-// Routes
-app.use(routes);
 
 // Serve index.html for root route
 app.get('/', (req, res) => {
@@ -26,3 +22,4 @@ app.get('/notes', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on PORT: ${PORT}`);
 });
+
